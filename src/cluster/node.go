bcd483786e4a89941d1322077cd645bdd33afdf3
Blake Eggleston <bdeggleston@gmail.com>
2013-10-02 21:13:31 -0700

    adding some boilerplate methods to the node structs

--- src/cluster/node.go
+++ src/cluster/node.go
@@ -10,31 +10,78 @@ package cluster
 import (
 	"time"
 	"store"
+
+	"code.google.com/p/go-uuid/uuid"
 )
+
+type NodeId uuid.UUID
 
 type node interface {
 	Name() string
 	GetToken() Token
-	GetId() string
+	GetId() NodeId
 
+	// starts up the node
+	start()
+
+	// executes a read instruction against the node's store
 	ExecuteRead(cmd string, key string, args []string)
+
+	// executes a write instruction against the node's store
 	ExecuteWrite(cmd string, key string, args []string, timestamp time.Time)
 }
 
+// the baseNode defines all of the properties
+// and methods in common among node types
 type baseNode struct {
 	name string
 	token Token
-	id string
+	id NodeId
 }
 
 
+func (n *baseNode) Name() string { return n.name }
+
+func (n *baseNode) GetToken() { return n.token }
+
+func (n *baseNode) GetId() { return n.id }
+
+// LocalNode provides access to the local store
 type LocalNode struct {
 	baseNode
 	store store.Store
 }
 
+func NewLocalNode(id NodeId, token Token, name string) (n *LocalNode) {
+	//
+	n.id = id
+	n.token = token
+	n.name = name
+	return
+}
+
+func (n *LocalNode) start() {
+	// connect the store
+}
+
+
+// RemoteNode communicates with other nodes in the cluster
 type RemoteNode struct {
 	baseNode
+
+	// the node's network address
+	addr string
 }
+
+
+func NewRemoteNode(addr string) (n *RemoteNode) {
+	n.addr = addr
+	return
+}
+
+func (n *RemoteNode) start() {
+	// connect to the node and get it's info
+}
+
 
 
