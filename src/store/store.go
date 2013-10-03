aaab333b252cb24032f07f379bf0ab6cec1845ff
Blake Eggleston <bdeggleston@gmail.com>
2013-10-02 20:46:39 -0700

    adding initial store struct

---
+++ src/store/store.go
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: bdeggleston
+ * Date: 10/2/13
+ * Time: 7:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package store
+
+type Store struct {
+
+}
