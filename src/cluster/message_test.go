9038402378684bee2506db5d7c4bc936d7c8e08c
Blake Eggleston <bdeggleston@gmail.com>
2013-10-08 21:55:43 -0700

    moving common test helpers into a base_test file

--- src/cluster/message_test.go
+++ src/cluster/message_test.go
@@ -9,22 +9,6 @@ import (
 )
 
 
-func equalityCheck(t *testing.T, name string, v1 interface {}, v2 interface{}) {
-	if v1 != v2 {
-		t.Errorf("%v mismatch. Expecting %v, got %v", name, v1, v2)
-	} else {
-		t.Logf("%v OK: %v", name, v1)
-	}
-}
-
-func sliceEqualityCheck(t *testing.T, name string, v1 []byte, v2 []byte) {
-	if !bytes.Equal(v1, v2) {
-		t.Errorf("%v mismatch. Expecting %v, got %v", name, v1, v2)
-	} else {
-		t.Logf("%v OK: %v", name, v1)
-	}
-}
-
 func messageInterfaceCheck(_ Message) {}
 
 
