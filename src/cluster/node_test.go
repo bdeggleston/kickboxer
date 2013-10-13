658803561524fe4fdfa0c9fe300e2d4372f51eb2
Blake Eggleston <bdeggleston@gmail.com>
2013-10-12 17:46:21 -0700

    adding stubbed node tests

--- src/cluster/node_test.go
+++ src/cluster/node_test.go
@@ -8,6 +8,7 @@
 package cluster
 
 import (
+	"testing"
 	"time"
 )
 
@@ -28,6 +29,7 @@ type mockNode struct {
 	name string
 	token Token
 	id NodeId
+	status NodeStatus
 
 	isStarted bool
 	reads []readCall
@@ -36,6 +38,7 @@ type mockNode struct {
 
 func newMockNode(id NodeId, token Token, name string) (*mockNode) {
 	n := &mockNode{id:id, token:token, name:name}
+	n.status = NODE_UP
 	n.reads = make([]readCall, 0, 5)
 	n.writes = make([]writeCall, 0, 5)
 	return n
@@ -44,6 +47,8 @@ func newMockNode(id NodeId, token Token, name string) (*mockNode) {
 func (n *mockNode) Name() string { return n.name }
 
 func (n *mockNode) GetToken() Token { return n.token }
+
+func (n *mockNode) GetStatus() NodeStatus { return n.status }
 
 func (n *mockNode) GetId() NodeId { return n.id }
 
@@ -70,4 +75,58 @@ func (n *mockNode) ExecuteRead(cmd string, key string, args []string) {
 func (n *mockNode) ExecuteWrite(cmd string, key string, args []string, timestamp time.Time) {
 	call := writeCall{cmd:cmd, key:key, args:args, timestamp:timestamp}
 	n.writes = append(n.writes, call)
+}
+
+
+/************** local tests **************/
+
+// tests that starting the local node starts the local store
+func TestStartingConnectsToStore(t *testing.T) {
+
+}
+
+// tests that calling ExecuteRead calls the proper store method
+func TestReadRequestCallsCorrectStoreMethod(t *testing.T) {
+
+}
+
+// tests that calling ExecuteWrite calls the proper store method
+func TestWriteRequestCallsCorrectStoreMethod(t *testing.T) {
+
+}
+
+/************** remote tests **************/
+
+// tests that calling Start on a remote node initiates
+// a connection to the remote peer server
+func TestStartingConnectsToPeer(t *testing.T) {
+
+}
+
+// tests that sending and receiving messages works as
+// expected
+func TestMessageSendingSuccessCase(t *testing.T) {
+
+}
+
+// tests that a node is marked as down if sending
+// a message fails
+func TestMessageSendingFailureCase(t *testing.T) {
+
+}
+
+// tests that the handshake happens when
+// getting a newly constructed connection from the pool
+func TestGetConnectionPerformsHandshake(t *testing.T) {
+
+}
+
+// tests that calling ExecuteRead sends a ReadMessage
+func TestReadRequestSendsCorrectMessage(t *testing.T) {
+
+}
+
+// tests that calling ExecuteWrite sends a WriteMessage
+func TestWriteRequestSendsCorrectMessage(t *testing.T) {
+
 }
