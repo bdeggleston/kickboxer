e76297b4eacf3f544af461f200de50035e23ca61
Blake Eggleston <bdeggleston@gmail.com>
2013-10-14 07:21:54 -0700

    adding peer discovery method to cluster

--- src/cluster/cluster.go
+++ src/cluster/cluster.go
@@ -141,6 +141,70 @@ func (c *Cluster) getPeerData() *PeerData {
 	return nil
 }
 
+// talks to the seed addresses and any additional
+// remote nodes we're already aware of to discover
+// new node
+func (c* Cluster) discoverPeers() error {
+
+	// checks the existing nodes for the given address
+	addrIsKnown := func(addr string) *RemoteNode {
+		for _, v := range c.ring.AllNodes() {
+			if node, ok := v.(*RemoteNode); ok {
+				if node.addr == addr {
+					return node
+				}
+			}
+		}
+		return nil
+	}
+
+	// add seed nodes
+	for _, addr := range c.seeds {
+		if node := addrIsKnown(addr); node == nil {
+			node := NewRemoteNode(addr, c)
+			// start the node to get it's info
+			if err := node.Start(); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			c.ring.AddNode(node)
+		}
+	}
+
+	// get peer info from existing nodes
+	getRemoteNodes := func() []*RemoteNode {
+		peers := make([]*RemoteNode, 0)
+		for _, v := range c.ring.AllNodes() {
+			if node, ok := v.(*RemoteNode); ok {
+				peers = append(peers, node)
+			}
+		}
+		return peers
+	}
+	peers := getRemoteNodes()
+	request := &DiscoverPeersRequest{NodeId:c.GetNodeId()}
+	for _, node := range peers {
+		// don't add yourself
+		if node.GetId() == c.GetNodeId() {
+			continue
+		}
+		response, _, err := node.sendMessage(request)
+		if err != nil { return err }
+		peerMessage, ok := response.(*DiscoverPeerResponse)
+		if !ok {
+			return fmt.Errorf("Unexpected message type. Expected *DiscoverPeerResponse, got %T", response)
+		}
+		for _, peer := range peerMessage.Peers {
+			n := NewRemoteNodeInfo(peer.NodeId, peer.Token, peer.Name, peer.Addr, c)
+			if err := c.ring.AddNode(n); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
 func (c* Cluster) Start() error {
 	// start listening for connections
 	if err := c.peerServer.Start(); err != nil {
@@ -157,9 +221,9 @@ func (c* Cluster) Start() error {
 	}
 
 	// check for additional nodes
-//	if err := c.discoverPeers(); err != nil {
-//		return err
-//	}
+	if err := c.discoverPeers(); err != nil {
+		return err
+	}
 
 	c.status = CLUSTER_NORMAL
 
