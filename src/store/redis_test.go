6fe20affed3c558288779ce991cb35702bf4bdb5
Blake Eggleston <bdeggleston@gmail.com>
2013-10-16 08:11:43 -0700

    adding tests around read/write identification

--- src/store/redis_test.go
+++ src/store/redis_test.go
@@ -1,2 +1,42 @@
 package store
 
+import (
+	"testing"
+)
+
+// table of instructions, and whether they're
+// a write (true) or read (false)
+var isWrite = []struct {
+	cmd string
+	result bool
+}{
+	{"GET", false},
+	{"SET", true},
+	{"DEL", true},
+}
+
+func TestIsWriteCmd(t *testing.T) {
+	r := &Redis{}
+	for _, c := range isWrite {
+		if result := r.IsWriteCommand(c.cmd); result != c.result {
+			if result {
+				t.Errorf("%v erroneously identified as a write", c.cmd)
+			} else {
+				t.Errorf("%v not identified as a write", c.cmd)
+			}
+		}
+	}
+}
+
+func TestIsReadCmd(t *testing.T) {
+	r := &Redis{}
+	for _, c := range isWrite {
+		if result := r.IsReadCommand(c.cmd); result != !c.result {
+			if result {
+				t.Errorf("%v erroneously identified as a read", c.cmd)
+			} else {
+				t.Errorf("%v not identified as a read", c.cmd)
+			}
+		}
+	}
+}
