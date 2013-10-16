07fc7415185dd155e7c14255c0ed80597b1ff130
Blake Eggleston <bdeggleston@gmail.com>
2013-10-16 07:31:02 -0700

    colorizing equality check failures

--- src/cluster/base_test.go
+++ src/cluster/base_test.go
@@ -9,17 +9,17 @@ import (
 
 func equalityCheck(t *testing.T, name string, v1 interface {}, v2 interface{}) {
 	if v1 != v2 {
-		t.Errorf("%v mismatch. Expecting %v, got %v", name, v1, v2)
+		t.Errorf("\x1b[1m\x1b[35m%v mismatch. Expecting [%v], got [%v]\x1b[0m", name, v1, v2)
 	} else {
-		t.Logf("%v OK: %v", name, v1)
+		t.Logf("%v OK: [%v]", name, v1)
 	}
 }
 
 func sliceEqualityCheck(t *testing.T, name string, v1 []byte, v2 []byte) {
 	if !bytes.Equal(v1, v2) {
-		t.Errorf("%v mismatch. Expecting %v, got %v", name, v1, v2)
+		t.Errorf("\x1b[1m\x1b[35m%v mismatch. Expecting [%v], got [%v]\x1b[0m", name, v1, v2)
 	} else {
-		t.Logf("%v OK: %v", name, v1)
+		t.Logf("%v OK: [%v]", name, v1)
 	}
 }
 
