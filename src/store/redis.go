a2fedaf23520ccefc840a32c2b731767738086d1
Blake Eggleston <bdeggleston@gmail.com>
2013-10-16 08:11:32 -0700

    implementing read/write identifiers

--- src/store/redis.go
+++ src/store/redis.go
@@ -3,6 +3,7 @@ package store
 import (
 //	"bufio"
 //	"reflect"
+	"strings"
 	"time"
 )
 
@@ -10,6 +11,17 @@ type simpleValue struct {
 	data string
 	time time.Time
 }
+
+// read instructions
+const (
+	GET		= "GET"
+)
+
+// write instructions
+const (
+	SET		= "SET"
+	DEL		= "DEL"
+)
 
 
 type Redis struct {
@@ -26,3 +38,26 @@ func (s *Redis) Stop() error {
 	return nil
 }
 
+func (s *Redis) ExecuteRead(cmd string, key string, args []string) (*Value, error) {
+	return nil, nil
+}
+
+func (s *Redis) ExecuteWrite(cmd string, key string, args []string, timestamp time.Time) (*Value, error) {
+	return nil, nil
+}
+
+func (s *Redis) IsReadCommand(cmd string) bool {
+	switch strings.ToUpper(cmd) {
+	case GET:
+		return true
+	}
+	return false
+}
+
+func (s *Redis) IsWriteCommand(cmd string) bool {
+	switch strings.ToUpper(cmd) {
+	case SET, DEL:
+		return true
+	}
+	return false
+}
