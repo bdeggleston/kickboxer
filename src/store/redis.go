3bde8a79c8c3bd3721c5036bd65dbcb805fdfad1
Blake Eggleston <bdeggleston@gmail.com>
2013-10-16 21:57:58 -0700

    implementing value interface for simpleValue, starting Execute functions for store

--- src/store/redis.go
+++ src/store/redis.go
@@ -1,16 +1,68 @@
 package store
 
 import (
-//	"bufio"
-//	"reflect"
+	"bufio"
+	"fmt"
 	"strings"
 	"sync"
 	"time"
 )
 
-type simpleValue struct {
+import (
+	"serializer"
+)
+
+const (
+	SINGLE_VALUE = ValueType("SINGLE")
+)
+
+// a single value used for
+// key/val types
+type singleValue struct {
 	data string
 	time time.Time
+}
+
+// single value constructor
+func newSingleValue(data string, time time.Time) *singleValue {
+	v := &singleValue{
+		data:data,
+		time:time,
+	}
+	return v
+}
+
+func (v *singleValue) GetTimestamp() time.Time {
+	return v.time
+}
+
+func (v *singleValue) GetValueType() ValueType {
+	return SINGLE_VALUE
+}
+
+func (v *singleValue) Serialize(buf *bufio.Writer) error {
+	if err := serializer.WriteFieldBytes(buf, []byte(v.data)); err != nil {
+		return err
+	}
+	if err := serializer.WriteTime(buf, v.time); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v *singleValue) Deserialize(buf *bufio.Reader) error {
+	if val, err := serializer.ReadFieldBytes(buf); err != nil {
+		return err
+	} else {
+		v.data = string(val)
+	}
+
+	if t, err := serializer.ReadTime(buf); err != nil {
+		return err
+	} else {
+		v.time = t
+	}
+	return nil
 }
 
 // read instructions
@@ -48,12 +100,29 @@ func (s *Redis) Stop() error {
 func (s *Redis) ExecuteRead(cmd string, key string, args []string) (*Value, error) {
 	s.lock.RLock()
 	defer s.lock.RUnlock()
+
+	switch cmd {
+	case GET:
+		//
+	default:
+		return nil, fmt.Errorf("Unrecognized read command: %v", cmd)
+	}
+
 	return nil, nil
 }
 
 func (s *Redis) ExecuteWrite(cmd string, key string, args []string, timestamp time.Time) (*Value, error) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
+
+	switch cmd {
+	case SET:
+		//
+	case DEL:
+		//
+	default:
+		return nil, fmt.Errorf("Unrecognized read command: %v", cmd)
+	}
 	return nil, nil
 }
 
