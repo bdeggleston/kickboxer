3207b39bedfd772aee258cb7bf59508fa1423fea
Blake Eggleston <bdeggleston@gmail.com>
2013-10-16 22:18:59 -0700

    adding test around redis single value

--- src/store/redis_test.go
+++ src/store/redis_test.go
@@ -1,7 +1,10 @@
 package store
 
 import (
+	"bytes"
 	"testing"
+	"testing_helpers"
+	"time"
 )
 
 // table of instructions, and whether they're
@@ -14,6 +17,9 @@ var isWrite = []struct {
 	{"SET", true},
 	{"DEL", true},
 }
+
+// compile time assertion that Value is implemented
+func valueInterfaceCheck(v Value) {}
 
 func TestIsWriteCmd(t *testing.T) {
 	r := &Redis{}
@@ -39,4 +45,32 @@ func TestIsReadCmd(t *testing.T) {
 			}
 		}
 	}
+}
+
+/***************** value tests *****************/
+
+// tests the single value
+func TestSingleValue(t *testing.T) {
+	buf := &bytes.Buffer{}
+	src := newSingleValue("blake", time.Now())
+	valueInterfaceCheck(src)
+
+	if err := WriteRedisValue(buf, src); err != nil {
+		t.Fatalf("Unexpected serialization error: %v", err)
+	}
+
+	val, vtype, err := ReadRedisValue(buf)
+	if err != nil {
+		t.Fatalf("Unexpected deserialization error: %v", err)
+	}
+	if vtype != SINGLE_VALUE {
+		t.Fatalf("Unexpected value type enum: %v", vtype)
+	}
+	dst, ok := val.(*singleValue)
+	if !ok {
+		t.Fatalf("Unexpected value type: %T", val)
+	}
+
+	testing_helpers.AssertEqual(t, "data", src.data, dst.data)
+	testing_helpers.AssertEqual(t, "time", src.time, dst.time)
 }
