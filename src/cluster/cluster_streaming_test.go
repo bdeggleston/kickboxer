fa439bebc6c35c500552065557826f326ab21e4b
Blake Eggleston <bdeggleston@gmail.com>
2013-10-20 14:09:28 -0700

    adding stubbed tests for query routing during streaming

---
+++ src/cluster/cluster_streaming_test.go
@@ -0,0 +1,21 @@
+package cluster
+
+import (
+	"testing"
+)
+
+/************** query behavior tests **************/
+
+// writes should be routed to the streaming node and mirrored to the local node
+func TestStreamingWriteRouting(t *testing.T) {
+
+}
+
+// reads should be routed directly to the streaming nodes
+func TestStreamingReadRouting(t *testing.T) {
+
+}
+
+/************** streaming data tests **************/
+
+
