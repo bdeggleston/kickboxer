3de88ba324c24362e72728033ba14012ec52aa9e
Blake Eggleston <bdeggleston@gmail.com>
2013-10-19 20:32:44 -0700

    adding stubbed tests for del command

--- src/redis/store_test.go
+++ src/redis/store_test.go
@@ -133,3 +133,17 @@ func TestGetValidation(t *testing.T) {
 		t.Errorf("Expected error, got nil")
 	}
 }
+
+func TestDel(t *testing.T) {
+
+}
+
+// tests that the correct number of deleted keys are returned
+func TestDelReturnVal(t *testing.T) {
+
+}
+
+// tests validation of DEL insructions
+func TestDelValidation(t *testing.T) {
+
+}
