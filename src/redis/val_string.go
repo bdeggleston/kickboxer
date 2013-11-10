a4200e347f1ae7e08641c557c0e2bb32faacd784
Blake Eggleston <bdeggleston@gmail.com>
2013-11-09 21:16:32 -0800

    adding string value reconciliation method

--- src/redis/val_string.go
+++ src/redis/val_string.go
@@ -64,6 +64,39 @@ func (v *String) Deserialize(buf *bufio.Reader) error {
 	return nil
 }
 
-func reconcileString(values map[string] *String) (*String, map[string][]*store.Instruction, error) {
-	return nil, nil, nil
+func reconcileString(key string, values map[string]store.Value) (*String, map[string][]*store.Instruction, error) {
+	// workout the value with the highest timestamp
+	strMap := make(map[string]*String, len(values))
+	var highTimestamp time.Time
+	var highValue *String
+	var highNodeId string
+	for nodeid, val := range values {
+		if val.GetValueType() != STRING_VALUE {
+			strMap[nodeid] = nil
+		} else {
+			sval := val.(*String)
+			if ts := sval.GetTimestamp(); ts.After(highTimestamp) {
+				highTimestamp = ts
+				highNodeId = nodeid
+				highValue = sval
+			}
+			strMap[nodeid] = sval
+		}
+	}
+
+	// create instructions for the unequal nodes
+	instructions := make(map[string][]*store.Instruction)
+	for nodeid, val := range strMap {
+		if nodeid == highNodeId || val == highValue {
+			continue
+		}
+		instructions[nodeid] = []*store.Instruction{&store.Instruction{
+			Cmd:"SET",
+			Key:key,
+			Args:[]string{highValue.data},
+			Timestamp:highValue.time,
+		}}
+	}
+
+	return highValue, instructions, nil
 }
