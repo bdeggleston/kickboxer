01f7b566449819b1426f78d803381b3d733d0432
Blake Eggleston <bdeggleston@gmail.com>
2013-11-22 18:04:59 -0800

    adding stubbed test file for datacenter join

---
+++ src/cluster/cluster_datacenter_join_test.go
@@ -0,0 +1,10 @@
+/**
+ What happens when a new datacenter joins?
+ * both clusters collect information about each other
+ * streaming needs to be reworked to reconcile incoming data
+
+ Cases to handle:
+ 	* both dcs have data, data needs to be reconciled between both datacenters
+ */
+package cluster
+
