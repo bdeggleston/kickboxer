70cc904f4b91d5bada34d7885aa8acc6721e50fe
Blake Eggleston <bdeggleston@gmail.com>
2013-11-24 10:08:37 -0800

    adding methods and tests to datacenter container

--- src/cluster/datacenter.go
+++ src/cluster/datacenter.go
@@ -1,6 +1,7 @@
 package cluster
 
 import (
+	"fmt"
 	"sync"
 )
 
@@ -33,13 +34,37 @@ func NewDatacenterContainer() *DatacenterContainer {
 	return dc
 }
 
+func (dc *DatacenterContainer) AddNode(node Node) error {
+	dc.lock.Lock()
+	defer dc.lock.Unlock()
+
+	dcId := node.GetDatacenterId()
+	if _, exists := dc.rings[dcId]; !exists {
+		dc.rings[dcId] = NewRing()
+	}
+	return dc.rings[dcId].AddNode(node)
+}
+
+func (dc *DatacenterContainer) GetRing(dcId DatacenterId) (*Ring, error) {
+	dc.lock.RLock()
+	defer dc.lock.RUnlock()
+
+	ring, exists := dc.rings[dcId]
+	if !exists {
+		return nil, fmt.Errorf("Unknown datacenter [%v]", dcId)
+	}
+	return ring, nil
+}
+
 func (dc *DatacenterContainer) GetNodesForToken(t Token, replicationFactor uint32) []Node {
 	dc.lock.RLock()
 	defer dc.lock.RUnlock()
+
 	nodes := make([]Node, 0, int(replicationFactor) * len(dc.rings))
 	for _, ring := range dc.rings {
-//		nodes = nodes + ring.GetNodesForToken(t, replicationFactor)
-		_ = ring
+		for _, node := range ring.GetNodesForToken(t, replicationFactor) {
+			nodes = append(nodes, node)
+		}
 	}
 
 	return nodes
--- src/cluster/datacenter_test.go
+++ src/cluster/datacenter_test.go
@@ -1,2 +1,83 @@
 package cluster
 
+import (
+	"fmt"
+	"testing"
+	"testing_helpers"
+)
+
+func setupDC() *DatacenterContainer {
+	dc := NewDatacenterContainer()
+
+	for _, dcid := range []DatacenterId{"DC1", "DC2", "DC3"} {
+		for i:=0; i<10; i++ {
+			n := newMockNode(
+				NewNodeId(),
+				dcid,
+				Token([]byte{0,0,byte(i),0}),
+				fmt.Sprintf("N%v", i),
+			)
+			dc.AddNode(n)
+		}
+	}
+	return dc
+}
+
+// tests add node behavior
+func TestAddNode(t *testing.T) {
+	dc := setupDC()
+
+	testing_helpers.AssertEqual(t, "num dcs", 3, len(dc.rings))
+	for i, dcid := range []DatacenterId{"DC1", "DC2", "DC3"} {
+		dcNum := i + 1
+		ring, exists := dc.rings[dcid]
+		if !exists {
+			t.Fatalf("DC not found: [%v]", dcid)
+		}
+		nodes := ring.AllNodes()
+		testing_helpers.AssertEqual(t, "ring size", 10, len(nodes))
+		node := nodes[0]
+
+		testing_helpers.AssertEqual(t, "dc name", DatacenterId(fmt.Sprintf("DC%v", dcNum)), node.GetDatacenterId())
+	}
+}
+
+func TestGetRing(t *testing.T) {
+	dc := setupDC()
+
+	ring, err := dc.GetRing("DC1")
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	if ring == nil {
+		t.Fatalf("Ring unexpectedly nil")
+	}
+
+	ring, err = dc.GetRing("DC5")
+	if err == nil {
+		t.Fatalf("Expected error, got nil")
+	}
+	if ring != nil {
+		t.Fatalf("Expected nil ring")
+	}
+}
+
+func TestGetNodesForToken(t *testing.T) {
+	dc := setupDC()
+
+	var token Token
+
+	token = Token([]byte{0,0,4,5})
+	nodes := dc.GetNodesForToken(token, 3)
+
+	if len(nodes) != 9 { t.Fatalf("wrong number of nodes returned, expected 3, got %v", len(nodes)) }
+	for i:=0; i<3; i++ {
+		baseIdx := i * 3
+		dcid := nodes[baseIdx].GetDatacenterId()
+		testing_helpers.AssertEqual(t, "node[0]", dc.rings[dcid].tokenRing[5].GetId(), nodes[baseIdx + 0].GetId())
+		testing_helpers.AssertEqual(t, "node[1]", dc.rings[dcid].tokenRing[6].GetId(), nodes[baseIdx + 1].GetId())
+		testing_helpers.AssertEqual(t, "node[2]", dc.rings[dcid].tokenRing[7].GetId(), nodes[baseIdx + 2].GetId())
+	}
+}
+
+
