1d54ce5d73f3157077d85ae25aeef524c723976c
Blake Eggleston <bdeggleston@gmail.com>
2013-12-08 18:27:23 -0800

    adding result from testing helper assertions

--- src/testing_helpers/helpers.go
+++ src/testing_helpers/helpers.go
@@ -5,18 +5,22 @@ import (
 	"testing"
 )
 
-func AssertEqual(t *testing.T, name string, v1 interface {}, v2 interface{}) {
+func AssertEqual(t *testing.T, name string, v1 interface {}, v2 interface{}) bool {
 	if v1 != v2 {
 		t.Errorf("\x1b[1m\x1b[35m%v mismatch. Expecting [%v], got [%v]\x1b[0m", name, v1, v2)
+		return false
 	} else {
 		t.Logf("%v OK: [%v]", name, v1)
 	}
+	return true
 }
 
-func AssertSliceEqual(t *testing.T, name string, v1 []byte, v2 []byte) {
+func AssertSliceEqual(t *testing.T, name string, v1 []byte, v2 []byte) bool {
 	if !bytes.Equal(v1, v2) {
 		t.Errorf("\x1b[1m\x1b[35m%v mismatch. Expecting [%v], got [%v]\x1b[0m", name, v1, v2)
+		return false
 	} else {
 		t.Logf("%v OK: [%v]", name, v1)
 	}
+	return true
 }
