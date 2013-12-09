e159e450184c1fda7a315007be4363e6310f21fa
Blake Eggleston <bdeggleston@gmail.com>
2013-12-08 18:36:42 -0800

    adding string array comparison helper

--- src/testing_helpers/helpers.go
+++ src/testing_helpers/helpers.go
@@ -24,3 +24,21 @@ func AssertSliceEqual(t *testing.T, name string, v1 []byte, v2 []byte) bool {
 	}
 	return true
 }
+
+func AssertStringArrayEqual(t *testing.T, name string, v1 []string, v2 []string) bool {
+	areEqual := true
+	areEqual = areEqual && (len(v1) == len(v2))
+	if areEqual {
+		for i:=0; i<len(v1); i++ {
+			areEqual = areEqual && (v1[i] == v2[i])
+		}
+	}
+
+	if !areEqual {
+		t.Errorf("\x1b[1m\x1b[35m%v mismatch. Expecting [%v], got [%v]\x1b[0m", name, v1, v2)
+		return false
+	} else {
+		t.Logf("%v OK: [%v]", name, v1)
+	}
+	return true
+}
