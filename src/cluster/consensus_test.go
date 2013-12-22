e9faa60776ee82d40c7aac59acca52ceba88b744
Blake Eggleston <bdeggleston@gmail.com>
2013-12-22 12:25:42 -0800

    adding stubbed consensus struct tests

--- src/cluster/consensus_test.go
+++ src/cluster/consensus_test.go
@@ -4,18 +4,38 @@ import (
 	"testing"
 )
 
-func TestSuccessCase(t *testing.T) {
+/** Consensus Manager **/
+
+// test that the consensus leader can accurately
+// determine if the local node is eligible to
+// be the command leader
+func TestCommandLeaderEligibility(t *testing.T) {
 
 }
 
-func TestInterferingOperation(t *testing.T) {
+// tests that ineligible commands are forwarded
+// to the correct nodes
+func TestCommandLeaderForwarding(t *testing.T) {
 
 }
 
-func TestNonInterferingOperation(t *testing.T) {
+// test that command leader forwarding is tolerant
+// of replicas being down
+func TestCommandLeaderForwardingFailureTolerance(t *testing.T) {
 
 }
 
-func TestCommitFailureRecovery(t *testing.T) {
+// test that the get or create instance method
+// works does that
+func TestInstanceRetrieval(t *testing.T) {
+
+}
+
+/** Consensus Instance **/
+
+// tests that the instance object is able to return
+// the proper replicas and quorum size for different
+// consistency levels
+func TestGetReplicas(t *testing.T) {
 
 }
