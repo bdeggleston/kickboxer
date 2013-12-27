608cddc4c0c3f14066fe3cfb2bd1b76a6478db4a
Blake Eggleston <bdeggleston@gmail.com>
2013-12-27 07:39:03 -0800

    adding instance creation to execution method

--- src/consensus/scope.go
+++ src/consensus/scope.go
@@ -20,6 +20,7 @@ type Scope struct {
 	executed   []InstanceID
 	maxSeq     uint64
 	lock       sync.RWMutex
+	cmdLock    sync.Mutex
 	manager    *Manager
 }
 
@@ -36,6 +37,50 @@ func NewScope(name string, manager *Manager) *Scope {
 
 func (s *Scope) GetLocalID() node.NodeId {
 	return s.manager.GetLocalID()
+}
+
+// persists the scope's state to disk
+func (s *Scope) Persist() error {
+	return nil
+}
+
+// creates an epaxos instance from the given instructions
+func (s *Scope) makeInstance(instructions []*store.Instruction) (*Instance, error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	// grab ALL instances as dependencies for now
+	// TODO: use fewer deps (inProgress + committed + executed[len(executed)-1])
+	executedDeps := 1
+	if len(s.executed) == 0 {
+		executedDeps = 0
+	}
+	deps := make([]InstanceID, len(s.inProgress) + len(s.committed) + executedDeps)
+	for dep := range s.inProgress { deps = append(deps, dep) }
+	for dep := range s.committed { deps = append(deps, dep) }
+	if executedDeps > 0 { deps = append(deps, s.executed[len(s.executed) - 1]) }
+
+	s.maxSeq++
+	seq := s.maxSeq
+
+	instance := &Instance{
+		InstanceID: NewInstanceID(),
+		LeaderID: s.GetLocalID(),
+		Commands: instructions,
+		Dependencies: deps,
+		Sequence: seq,
+		Status: INSTANCE_PREACCEPTED,
+	}
+
+	// add to manager maps
+	s.instances[instance.InstanceID] = instance
+	s.inProgress[instance.InstanceID] = instance
+
+	if err := s.Persist(); err != nil {
+		return nil, err
+	}
+
+	return instance, nil
 }
 
 func (s *Scope) ExecuteInstructions(instructions []*store.Instruction, replicas []node.Node) (store.Value, error) {
@@ -56,5 +101,10 @@ func (s *Scope) ExecuteInstructions(instructions []*store.Instruction, replicas 
 		return nil, fmt.Errorf("remote replica size != replicas - 1. Are there duplicates?")
 	}
 
+	// create epaxos instance
+	instance, err := s.makeInstance(instructions)
+	if err != nil { return nil, err }
+
+	_ = instance
 	return nil, nil
 }
