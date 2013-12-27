79b8022313f40edabe72cf035d7e48677821b956
Blake Eggleston <bdeggleston@gmail.com>
2013-12-27 09:12:46 -0800

    adding comments to messages

--- src/consensus/message.go
+++ src/consensus/message.go
@@ -22,14 +22,23 @@ func (m *messageCheat) GetType() uint32 { return 0 }
 
 type PreAcceptRequest struct {
 	messageCheat
+
 	// the scope name the message
 	// is going to
 	Scope    string
+
 	Instance *Instance
 }
 
 type PreAcceptResponse struct {
 	messageCheat
+
+	// indicates the remote node ignored the
+	// preaccept due to an out of date ballot
 	Accepted bool
+
+	// the instance info the remote node added
+	// to it's instances. Sequence and Dependencies
+	// may be different
 	Instance *Instance
 }
