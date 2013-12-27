f18cace788b0686103b9a43bbf005596f5f854fe
Blake Eggleston <bdeggleston@gmail.com>
2013-12-27 08:13:34 -0800

    temporarily working around the message interface requirements 

--- src/consensus/message.go
+++ src/consensus/message.go
@@ -1,10 +1,29 @@
 package consensus
 
+import (
+	"bufio"
+)
+
+
+import (
+	"message"
+)
+
 type ScopedMessage interface {
+	message.Message
 	GetScope() string
 }
 
+// cheats the message interface implementation
+// TODO: actually implement the message interface
+type messageCheat struct { }
+func (m *messageCheat) Serialize(*bufio.Writer) error { return nil }
+func (m *messageCheat) Deserialize(*bufio.Reader) error { return nil }
+func (m *messageCheat) GetType() uint32 { return 0 }
+
+
 type PreAcceptRequest struct {
+	messageCheat
 	// the scope name the message
 	// is going to
 	Scope    string
@@ -12,6 +31,7 @@ type PreAcceptRequest struct {
 }
 
 type PreAcceptResponse struct {
+	messageCheat
 	Accepted bool
 	Instance *Instance
 }
