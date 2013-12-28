0a6213bcece4c8e0095b09e0e308731a43050ac6
Blake Eggleston <bdeggleston@gmail.com>
2013-12-28 08:24:32 -0800

    adding a mock node for consensus

--- src/consensus/mocks.go
+++ src/consensus/mocks.go
@@ -1,6 +1,12 @@
 package consensus
 
 import (
+	"time"
+)
+
+
+import (
+	"message"
 	"node"
 	"store"
 )
@@ -19,3 +25,33 @@ func (c *mockCluster) GetID() node.NodeId { return c.id }
 func (c *mockCluster) GetStore() store.Store { return nil }
 
 
+type mockNode struct {
+	id node.NodeId
+
+	// tracks the queries executed
+	// against this node
+	queries []*store.Instruction
+
+	cluster *mockCluster
+	manager *Manager
+}
+
+func newMockNode() {
+	cluster := newMockCluster()
+	return &mockNode{
+		id: cluster.GetID(),
+		queries: []*store.Instruction{},
+		cluster:cluster,
+		manager:NewManager(cluster),
+	}
+}
+
+func (n *mockNode) GetId() node.NodeId { return n.id }
+func (n *mockNode) ExecuteQuery(cmd string, key string, args []string, timestamp time.Time) (store.Value, error) {
+	store.NewInstruction(cmd, key, args, timestamp)
+	return nil, nil
+}
+
+func (n *mockNode) SendMessage(msg message.Message) (message.Message, error) {
+	return n.manager.HandleMessage(msg)
+}
