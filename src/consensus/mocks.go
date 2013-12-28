91bc27be34ade7306ee3e4f36c842ea2492d8e10
Blake Eggleston <bdeggleston@gmail.com>
2013-12-28 08:29:18 -0800

    adding a replaceable message handler to mock node, to make it easier to test problems caused by internode communication

--- src/consensus/mocks.go
+++ src/consensus/mocks.go
@@ -24,6 +24,9 @@ func newMockCluster() *mockCluster {
 func (c *mockCluster) GetID() node.NodeId { return c.id }
 func (c *mockCluster) GetStore() store.Store { return nil }
 
+func mockNodeDefaultMessageHandler(mn *mockNode, msg message.Message) (message.Message, error) {
+	return mn.manager.HandleMessage(msg)
+}
 
 type mockNode struct {
 	id node.NodeId
@@ -34,24 +37,27 @@ type mockNode struct {
 
 	cluster *mockCluster
 	manager *Manager
+	messageHandler func(*mockNode, message.Message) (message.Message, error)
 }
 
-func newMockNode() {
+func newMockNode() *mockNode {
 	cluster := newMockCluster()
 	return &mockNode{
 		id: cluster.GetID(),
 		queries: []*store.Instruction{},
 		cluster:cluster,
 		manager:NewManager(cluster),
+		messageHandler: mockNodeDefaultMessageHandler,
 	}
 }
 
 func (n *mockNode) GetId() node.NodeId { return n.id }
+
 func (n *mockNode) ExecuteQuery(cmd string, key string, args []string, timestamp time.Time) (store.Value, error) {
 	store.NewInstruction(cmd, key, args, timestamp)
 	return nil, nil
 }
 
 func (n *mockNode) SendMessage(msg message.Message) (message.Message, error) {
-	return n.manager.HandleMessage(msg)
+	return n.messageHandler(n, msg)
 }
