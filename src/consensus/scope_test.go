00498118eda8905a9299d042942883e585ab7904
Blake Eggleston <bdeggleston@gmail.com>
2013-12-27 22:29:14 -0800

    adding test around creating instances without executed instances

--- src/consensus/scope_test.go
+++ src/consensus/scope_test.go
@@ -11,8 +11,32 @@ import (
 )
 // tests that scope doesn't try to add executed instances
 // if no instances have been executed yet
-func TestInstanceCreationNoExecutions(t *testing.T) {
+func TestGetDepsNoExecutions(t *testing.T) {
+	scope := NewScope("a", nil)
+	expectedDeps := []InstanceID{}
+	for i:=0; i<4; i++ {
+		id := NewInstanceID()
+		expectedDeps = append(expectedDeps, id)
+		scope.inProgress[id] = nil
+	}
+	for i:=0; i<4; i++ {
+		id := NewInstanceID()
+		expectedDeps = append(expectedDeps, id)
+		scope.committed[id] = nil
+	}
 
+	// sanity checks
+	testing_helpers.AssertEqual(t, "inProgress len", 4, len(scope.inProgress))
+	testing_helpers.AssertEqual(t, "committed len", 4, len(scope.committed))
+	testing_helpers.AssertEqual(t, "executed len", 0, len(scope.executed))
+
+	expected := NewInstanceIDSet(expectedDeps)
+	actual := NewInstanceIDSet(scope.getCurrentDepsUnsafe())
+
+	testing_helpers.AssertEqual(t, "set lengths", len(expected), len(actual))
+	if !expected.Equal(actual) {
+		t.Errorf("Expected deps do not match actual deps.\nExpected: %v\nGot: %v", expected, actual)
+	}
 }
 
 func TestInstanceCreationPersistenceError(t *testing.T) {
@@ -64,6 +88,7 @@ func TestGetNextSeq(t *testing.T) {
 func TestInstanceCreation(t *testing.T) {
 	// TODO: test max seq assignment and scope update
 	// TODO: test dependency selection
+	// TODO: test new instances are added to inProgress
 	//	scope := NewScope("a", nil)
 }
 
