d7678e56aa839960eca1b0f45f8b829c7b6c9055
Blake Eggleston <bdeggleston@gmail.com>
2013-12-27 21:18:44 -0800

    adding accept and commit requests/responses

--- src/consensus/message.go
+++ src/consensus/message.go
@@ -13,6 +13,11 @@ type ScopedMessage interface {
 	GetScope() string
 }
 
+type BallotMessage interface {
+	message.Message
+	GetBallot() uint32
+}
+
 // cheats the message interface implementation
 // TODO: actually implement the message interface
 type messageCheat struct { }
@@ -37,8 +42,61 @@ type PreAcceptResponse struct {
 	// preaccept due to an out of date ballot
 	Accepted bool
 
+	// the highest ballot that's been seen
+	// for this instance
+	MaxBallot uint32
+
 	// the instance info the remote node added
 	// to it's instances. Sequence and Dependencies
 	// may be different
 	Instance *Instance
+
+	// if the command leader seems to be missing
+	// instances in it's deps, return them here
+	MissingInstances []*Instance
+}
+
+func (m *PreAcceptResponse) GetBallot() uint32 { return m.MaxBallot }
+
+type AcceptRequest struct {
+	messageCheat
+
+	// the scope name the message
+	// is going to
+	Scope    string
+
+	Instance *Instance
+
+	// if the replica seems to be missing
+	// instances in it's deps, send them here
+	MissingInstances []*Instance
+}
+
+type AcceptResponse struct {
+	messageCheat
+
+	// indicates the remote node ignored the
+	// preaccept due to an out of date ballot
+	Accepted bool
+
+	// the highest ballot that's been seen
+	// for this instance
+	MaxBallot uint32
+}
+
+func (m *AcceptResponse) GetBallot() uint32 { return m.MaxBallot }
+
+type CommitRequest struct {
+	messageCheat
+
+	// the scope name the message
+	// is going to
+	Scope    string
+
+	// the id of the messages to be committed
+	InstanceID InstanceID
+}
+
+type CommitResponse struct {
+	messageCheat
 }
