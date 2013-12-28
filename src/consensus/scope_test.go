8e7252802750c58ccbcdf650a0877d214bde4178
Blake Eggleston <bdeggleston@gmail.com>
2013-12-27 22:38:10 -0800

    condensing scope setup into function

--- src/consensus/scope_test.go
+++ src/consensus/scope_test.go
@@ -9,20 +9,65 @@ import (
 //	"store"
 	"testing_helpers"
 )
+
+func setupScope() *Scope {
+	scope := NewScope("a", nil)
+	for i:=0; i<4; i++ {
+		scope.inProgress[NewInstanceID()] = nil
+	}
+	for i:=0; i<4; i++ {
+		scope.committed[NewInstanceID()] = nil
+	}
+	for i:=0; i<4; i++ {
+		scope.executed = append(scope.executed, NewInstanceID())
+	}
+	return scope
+}
+
+// test that instances are created properly
+func TestInstanceCreation(t *testing.T) {
+	// TODO: test max seq assignment and scope update
+	// TODO: test dependency selection
+	// TODO: test new instances are added to inProgress
+	//	scope := NewScope("a", nil)
+}
+
+func TestGetCurrentDeps(t *testing.T) {
+	scope := setupScope()
+	expectedDeps := []InstanceID{}
+	for dep := range scope.inProgress {
+		expectedDeps = append(expectedDeps, dep)
+	}
+	for dep := range scope.committed {
+		expectedDeps = append(expectedDeps, dep)
+	}
+	expectedDeps = append(expectedDeps, scope.executed[len(scope.executed) - 1])
+
+	// sanity checks
+	testing_helpers.AssertEqual(t, "inProgress len", 4, len(scope.inProgress))
+	testing_helpers.AssertEqual(t, "committed len", 4, len(scope.committed))
+	testing_helpers.AssertEqual(t, "executed len", 4, len(scope.executed))
+
+	expected := NewInstanceIDSet(expectedDeps)
+	actual := NewInstanceIDSet(scope.getCurrentDepsUnsafe())
+
+	testing_helpers.AssertEqual(t, "set lengths", len(expected), len(actual))
+	if !expected.Equal(actual) {
+		t.Errorf("Expected deps do not match actual deps.\nExpected: %v\nGot: %v", expected, actual)
+	}
+}
+
 // tests that scope doesn't try to add executed instances
 // if no instances have been executed yet
 func TestGetDepsNoExecutions(t *testing.T) {
-	scope := NewScope("a", nil)
+	scope := setupScope()
+	scope.executed = []InstanceID{}
 	expectedDeps := []InstanceID{}
-	for i:=0; i<4; i++ {
-		id := NewInstanceID()
-		expectedDeps = append(expectedDeps, id)
-		scope.inProgress[id] = nil
+	for dep := range scope.inProgress {
+		expectedDeps = append(expectedDeps, dep)
 	}
-	for i:=0; i<4; i++ {
-		id := NewInstanceID()
-		expectedDeps = append(expectedDeps, id)
-		scope.committed[id] = nil
+	for dep := range scope.committed {
+		expectedDeps = append(expectedDeps, dep)
 	}
 
 	// sanity checks
@@ -39,44 +84,6 @@ func TestGetDepsNoExecutions(t *testing.T) {
 	}
 }
 
-func TestInstanceCreationPersistenceError(t *testing.T) {
-
-}
-
-func TestGetCurrentDeps(t *testing.T) {
-	scope := NewScope("a", nil)
-	expectedDeps := []InstanceID{}
-	for i:=0; i<4; i++ {
-		id := NewInstanceID()
-		expectedDeps = append(expectedDeps, id)
-		scope.inProgress[id] = nil
-	}
-	for i:=0; i<4; i++ {
-		id := NewInstanceID()
-		expectedDeps = append(expectedDeps, id)
-		scope.committed[id] = nil
-	}
-	for i:=0; i<4; i++ {
-		id := NewInstanceID()
-		scope.executed = append(scope.executed, id)
-		if i == 3 {
-			expectedDeps = append(expectedDeps, id)
-		}
-	}
-
-	// sanity checks
-	testing_helpers.AssertEqual(t, "inProgress len", 4, len(scope.inProgress))
-	testing_helpers.AssertEqual(t, "committed len", 4, len(scope.committed))
-	testing_helpers.AssertEqual(t, "executed len", 4, len(scope.executed))
-
-	expected := NewInstanceIDSet(expectedDeps)
-	actual := NewInstanceIDSet(scope.getCurrentDepsUnsafe())
-
-	testing_helpers.AssertEqual(t, "set lengths", len(expected), len(actual))
-	if !expected.Equal(actual) {
-		t.Errorf("Expected deps do not match actual deps.\nExpected: %v\nGot: %v", expected, actual)
-	}
-}
 func TestGetNextSeq(t *testing.T) {
 	scope := NewScope("a", nil)
 	scope.maxSeq = 5
@@ -84,11 +91,7 @@ func TestGetNextSeq(t *testing.T) {
 	testing_helpers.AssertEqual(t, "nextSeq", uint64(6), nextSeq)
 }
 
-// test that instances are created properly
-func TestInstanceCreation(t *testing.T) {
-	// TODO: test max seq assignment and scope update
-	// TODO: test dependency selection
-	// TODO: test new instances are added to inProgress
-	//	scope := NewScope("a", nil)
+func TestInstanceCreationPersistenceError(t *testing.T) {
+
 }
 
