7fcef7dfee9c297f65be2628ce5503535bb80a20
Blake Eggleston <bdeggleston@gmail.com>
2013-12-29 12:38:24 -0800

    adding stubbed tests around instance accept and commit

--- src/consensus/scope_accept_test.go
+++ src/consensus/scope_accept_test.go
@@ -4,17 +4,65 @@ import (
 	"testing"
 )
 
+/** acceptInstance **/
+
+// tests that an instance is marked as accepted,
+// added to the inProgress set, and persisted
+// if it doesn't have a higher status
+func TestAcceptInstanceSuccess(t *testing.T) {
+
+}
+
+// tests that an instance is not marked as accepted,
+// or added to the inProgress set if it already has
+// a higher status
+func TestAcceptInstanceHigherStatusFailure(t *testing.T) {
+
+}
+
+/** leader **/
+
+func TestSendAcceptSuccess(t *testing.T) {
+
+}
+
+func TestSendAcceptQuorumFailure(t *testing.T) {
+
+}
+
+func TestSendAcceptBallotFailure(t *testing.T) {
+	// TODO: figure out what to do in this situation
+	// the only way this would happen if is the command
+	// was taken over by another replica, in which case,
+	// should we just wait for the other leader to
+	// execute it?
+	t.Skip("figure out the expected behavior")
+}
+
+/** replica **/
+
+// test that instances are marked as accepted when
+// an accept request is received, and there are no
+// problems with the request
 func TestHandleAcceptSuccessCase(t *testing.T) {
 
 }
 
+// tests that accept messages fail if an higher
+// ballot number has been seen for this message
 func TestHandleAcceptOldBallotFailure(t *testing.T) {
+
+}
+
+// tests that handle accept adds any missing instances
+// in the missing instances message
+func TestHandleAcceptMissingInstanceBehavior(t *testing.T) {
 
 }
 
 // tests that accepts are handled properly if
 // the commit if for an instance the node has
-// not previously seen
+// not been previously seen by this replica
 func TestHandleAcceptUnknownInstance(t *testing.T) {
 
 }
--- src/consensus/scope_commit_test.go
+++ src/consensus/scope_commit_test.go
@@ -4,13 +4,36 @@ import (
 	"testing"
 )
 
-// tests that committing an instance moves the instance
-// from inProgress to committed
-func TestCommitMovesInstance(t *testing.T) {
+/** commitInstance **/
+
+// tests that an instance is marked as committed,
+// added to the committed set, removed from the
+// inProgress set, and persisted if it hasn't
+// already been executed
+func TestCommitInstanceSuccess(t *testing.T) {
 
 }
 
-func TestHandleCommit(t *testing.T) {
+// tests that an instance is not marked as committed,
+// or added to the committed set if it already has
+// been executed
+func TestCommitInstanceExecutedFailure(t *testing.T) {
+
+}
+
+/** leader **/
+
+// tests that calling sendCommit sends commit requests
+// to the other replicas
+func TestSendCommitSuccess(t *testing.T) {
+
+}
+
+/** replica **/
+
+// tests that an instance is marked as committed when
+// a commit request is recived
+func TestHandleCommitSuccess(t *testing.T) {
 
 }
 
