16b07909aeb390dfe6206364b3714c4200d0c6cc
Blake Eggleston <bdeggleston@gmail.com>
2014-01-11 09:34:13 -0800

    adding some stubbed management tests

---
+++ src/consensus/manager_test.go
@@ -0,0 +1,21 @@
+package consensus
+
+import (
+	"testing"
+)
+
+func TestGetScope(t *testing.T) {
+
+}
+
+func TestCheckScopeEligibility(t *testing.T) {
+
+}
+
+func TestGetScopeNodes(t *testing.T) {
+
+}
+
+func TestGetScopeReplicas(t *testing.T) {
+
+}
