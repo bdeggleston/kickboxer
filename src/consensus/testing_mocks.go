a6a136fbab232a692c82cf1444553abc7fbad5f9
Blake Eggleston <bdeggleston@gmail.com>
2014-01-14 21:29:42 -0800

    adding query handling to mock cluster

--- src/consensus/testing_mocks.go
+++ src/consensus/testing_mocks.go
@@ -1,6 +1,9 @@
 package consensus
 
 import (
+	"bufio"
+	"strconv"
+	"sync"
 	"time"
 )
 
@@ -10,15 +13,42 @@ import (
 	"store"
 )
 
+type intVal struct {
+	value int
+	time time.Time
+}
+
+func newIntVal(val int, ts time.Time) *intVal {
+	return &intVal{value:val, time:ts}
+}
+
+func (v *intVal) GetTimestamp() time.Time { return v.time }
+func (v *intVal) GetValueType() store.ValueType { return store.ValueType("INT") }
+
+func (v *intVal) Equal(value store.Value) bool {
+	if val, ok := value.(*intVal); ok {
+		return val.value == v.value
+	}
+	return false
+}
+
+func (v *intVal) Serialize(_ *bufio.Writer) error { return nil }
+func (v *intVal) Deserialize(_ *bufio.Reader) error { return nil }
+
 type mockCluster struct {
 	id node.NodeId
 	nodes []node.Node
+	lock sync.Mutex
+	instructions []*store.Instruction
+	values map[string]*intVal
 }
 
 func newMockCluster() *mockCluster {
 	return &mockCluster{
 		id: node.NewNodeId(),
 		nodes: make([]node.Node, 0, 10),
+		instructions: make([]*store.Instruction, 0),
+		values: make(map[string]*intVal),
 	}
 }
 
@@ -30,6 +60,22 @@ func (c *mockCluster) GetID() node.NodeId    { return c.id }
 func (c *mockCluster) GetStore() store.Store { return nil }
 func (c *mockCluster) GetNodesForKey(key string) []node.Node {
 	return c.nodes
+}
+
+func (c *mockCluster) ExecuteQuery(cmd string, key string, args []string, timestamp time.Time) (store.Value, error) {
+	panic("mockCluster doesn't implement Execute Query")
+}
+
+// executes a query against the local store
+func (c *mockCluster) ApplyQuery(cmd string, key string, args []string, timestamp time.Time) (store.Value, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	intVal, err := strconv.Atoi(args[0])
+	if err != nil { return nil, err }
+	val := newIntVal(intVal, timestamp)
+	c.values[key] = val
+	c.instructions = append(c.instructions, store.NewInstruction(cmd, key, args, timestamp))
+	return val, nil
 }
 
 func mockNodeDefaultMessageHandler(mn *mockNode, msg message.Message) (message.Message, error) {
