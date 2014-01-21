ea615c2a23eeb5dc89f2d80d85685525491c5ac6
Blake Eggleston <bdeggleston@gmail.com>
2014-01-21 08:06:14 -0800

    reworking out of date error to instance status update error

--- src/consensus/errors.go
+++ src/consensus/errors.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// returned if a request times out
 type TimeoutError struct {
 	message string
 }
@@ -14,6 +15,8 @@ func NewTimeoutError(format string, a ...interface{}) TimeoutError {
 	return TimeoutError{fmt.Sprintf(format, a...)}
 }
 
+// returned if a request fails due to
+// the request ballot being old
 type BallotError struct {
 	message string
 }
@@ -24,3 +27,30 @@ func NewBallotError(format string, a ...interface{}) BallotError {
 	return BallotError{fmt.Sprintf(format, a...)}
 }
 
+// returned if a request is aborted because
+// it's no longer valid (ie: running a prepare phase on a committed
+// instance)
+type InvalidStatusUpdateError struct {
+	InstanceID InstanceID
+	CurrentStatus InstanceStatus
+	ProposedStatus InstanceStatus
+}
+
+func (e InvalidStatusUpdateError) Error() string  { return e.String() }
+
+func (e InvalidStatusUpdateError) String() string {
+	return fmt.Sprintf(
+		"Cannot update status from %v to %v for instance %v",
+		e.CurrentStatus,
+		e.ProposedStatus,
+		e.InstanceID,
+	)
+}
+
+func NewInvalidStatusUpdateError(instance *Instance, update InstanceStatus) InvalidStatusUpdateError {
+	return InvalidStatusUpdateError{
+		InstanceID: instance.InstanceID,
+		CurrentStatus: instance.Status,
+		ProposedStatus: update,
+	}
+}
