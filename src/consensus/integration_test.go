c117238e25f332141f1a2cf506686f17ea9bacc2
Blake Eggleston <bdeggleston@gmail.com>
2014-01-30 21:33:49 -0800

    brutal integration test hacking

--- src/consensus/integration_test.go
+++ src/consensus/integration_test.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"math/rand"
+	"os"
 	"runtime"
 	"sync"
 	"time"
@@ -38,7 +39,8 @@ import (
 var _test_integration = flag.Bool("test.integration", false, "run the integration tests")
 var _test_show_progress = flag.Bool("test.progress", false, "run the integration tests")
 var _test_seed = flag.Int64("test.seed", 0, "the random seed to use")
-var _test_queries = flag.Int("test.queries", 300, "the number of queries to run")
+var _test_queries = flag.Int("test.queries", 1000, "the number of queries to run")
+var _test_replicas = flag.Int("test.replicas", 5, "the number of replicas in the test cluster")
 var _test_concurrent_queries = flag.Int("test.concurrent", 10, "the number of concurrent queries to run")
 
 func init() {
@@ -77,6 +79,7 @@ type opsCtrl struct {
 	msgChan chan opsMsgSendEvent
 	timeoutChan chan opsTimeoutEvent
 	simulateFailures bool
+	completed int
 }
 
 func (c *opsCtrl) messageHandler(node *mockNode, msg message.Message) (message.Message, error) {
@@ -87,7 +90,9 @@ func (c *opsCtrl) messageHandler(node *mockNode, msg message.Message) (message.M
 		reply: replyChan,
 	}
 	c.msgChan <- sendEvent
+	fmt.Printf("%T message put in channel\n", msg)
 	reply := <- replyChan
+	fmt.Printf("%T reply received from channel\n", reply.msg)
 	return reply.msg, reply.err
 }
 
@@ -152,7 +157,6 @@ func (c *opsCtrl) reactor() {
 	}
 
 	for i:=0; true; i++ {
-		time.Sleep(time.Duration(10) * time.Millisecond)
 		select {
 		case msgEvnt, open = <-c.msgChan:
 			if !open {
@@ -162,7 +166,7 @@ func (c *opsCtrl) reactor() {
 			if c.simulateFailures {
 				nid := msgEvnt.node.id
 				if partitionMap[nid] > 0 {
-					c.c.Logf("** Node %v is partitioned, skipping msg", indexMap[nid])
+					c.c.Logf("** Node %v is partitioned, skipping %T msg", indexMap[nid], msgEvnt.msg)
 					break
 				} else if (c.random.Uint32() % partionRatio) == 0 {
 					delay := partionLenBase
@@ -272,7 +276,11 @@ func (c *opsCtrl) reactor() {
 
 		if *_test_show_progress {
 			lastLen = len(instructionsSet)
-			for i:=0; i<lastLen; i++ {
+			start := 0
+			if len(instructionsSet) > 30 {
+				start = len(instructionsSet) - 30
+			}
+			for i:=start; i<lastLen; i++ {
 				for _, node := range c.nodes {
 					if len(node.cluster.instructions) < i + 1 {
 						fmt.Printf(" -- ")
@@ -285,19 +293,145 @@ func (c *opsCtrl) reactor() {
 			}
 		}
 
-		for n, node := range c.nodes {
-			// check that there's at least a little parity
-//			if len(instructionsSet) > 10 {
-//				c.c.Assert(len(node.cluster.instructions) > (len(instructionsSet) / 2), gocheck.Equals, true)
-//			}
-			if n == highNode { continue }
-			for i, instruction := range node.cluster.instructions {
-				if i + 1 > len(instructionsSet) {
-					continue
+		// check the order of instance execution
+		if true {
+			scopes := make([]*Scope, len(c.nodes))
+			for n, node := range c.nodes {
+				scopes[n] = node.manager.getScope("a")
+			}
+
+			// lock all scopes while testing
+			for _, scope := range scopes {
+				scope.lock.Lock()
+			}
+			numInst := make([]int, len(c.nodes))
+			for x, scope := range scopes {
+				numInst[x] = len(scope.instances)
+			}
+			fmt.Printf("%v <- known instances\n", numInst)
+
+			exMax := 0
+			exSizeMax := 0
+			for n, scope := range scopes {
+				if len(scope.executed) > exSizeMax {
+					exSizeMax = len(scope.executed)
+					exMax = n
 				}
-				c.c.Assert(instruction, gocheck.DeepEquals, instructionsSet[i], gocheck.Commentf("node: %v, idx: %v", n, i))
+			}
+
+			committed := NewInstanceIDSet([]InstanceID{})
+			for _, scp := range scopes {
+				for iid := range scp.committed {
+					committed.Add(iid)
+				}
+			}
+//			for iid := range committed {
+//				committed := make([]*Instance, 0)
+//				for _, scp := range scopes {
+//					if instance, ok := scp.committed[iid]; ok {
+//						committed = append(committed, instance)
+//					}
+//				}
+//
+//				if len(committed) > 0 {
+//					refInstance := committed[0]
+//					equal := true
+//					for _, instance := range committed[1:] {
+//						if !c.c.Check(refInstance.Sequence, gocheck.Equals, instance.Sequence) { equal = false }
+//						if !c.c.Check(refInstance.Dependencies, gocheck.DeepEquals, instance.Dependencies) { equal = false }
+//					}
+//					if !equal {
+//						for _, instance := range committed {
+//							fmt.Printf("Commit mismatch: %v %v %+v\n", instance.InstanceID, instance.Sequence, instance.Dependencies)
+//						}
+//						os.Exit(-3)
+//					}
+//				}
+//			}
+			_ = exMax
+//			for n := range c.nodes {
+//				if n == exMax { continue }
+//				expected := scopes[exMax].executed
+//				actual := scopes[n].executed
+//				if !c.c.Check(actual, gocheck.DeepEquals, expected[:len(actual)]) {
+//					fmt.Printf("%+v\n", expected[:len(actual)])
+//					fmt.Printf("%+v\n", actual)
+//					os.Exit(-2)
+//				}
+//			}
+			for n, node := range c.nodes {
+				if n == highNode { continue }
+				for i, instruction := range node.cluster.instructions {
+					if i + 1 > len(instructionsSet) {
+						continue
+					}
+					if !c.c.Check(instruction, gocheck.DeepEquals, instructionsSet[i], gocheck.Commentf("node: %v, idx: %v", n, i)){
+
+						scopes := make([]*Scope, len(c.nodes))
+						instances := make([]InstanceMap, len(c.nodes))
+						inst0 := 0
+						for i, inode := range c.nodes {
+							instances[i] = inode.manager.getScope("a").instances
+							scopes[i] = inode.manager.getScope("a")
+						}
+
+						for i := range c.nodes {
+							if i == inst0 { continue }
+							for iid := range instances[inst0] {
+								if _, ok := instances[i][iid]; !ok {
+									fmt.Printf("%v, %v not found\n", i, iid)
+									continue
+								}
+								expected := instances[inst0][iid]
+								actual := instances[i][iid]
+								exOexpected, err0 := scopes[inst0].getExecutionOrder(expected)
+								exOactual, err1 := scopes[i].getExecutionOrder(actual)
+								if err0 != nil || err1 != nil {
+									fmt.Printf("%v / %v", err0, err1)
+								} else {
+									fmt.Println(exOexpected)
+									fmt.Println(exOactual)
+									c.c.Check(exOactual, gocheck.DeepEquals, exOexpected)
+								}
+								if !NewInstanceIDSet(expected.Dependencies).Equal(NewInstanceIDSet(actual.Dependencies)) {
+									fmt.Printf("%v / %v mismatch\n", inst0, i)
+									fmt.Printf("%+v\n", instances[inst0][iid].Dependencies)
+									fmt.Printf("%+v\n", instances[i][iid].Dependencies)
+								}
+								if expected.Sequence != actual.Sequence {
+									fmt.Printf("%v / %v mismatch\n", inst0, i)
+									fmt.Printf("%+v\n", expected.Sequence)
+									fmt.Printf("%+v\n", actual.Sequence)
+								}
+							}
+						}
+
+						scope := node.manager.getScope("a")
+						localInst := scope.instances[scope.executed[len(scope.executed) - 1]]
+						//					fmt.Printf("Local:  %+v\n", localInst)
+
+						for _, onode := range c.nodes {
+							if onode.id == node.id {
+								continue
+							}
+							scope := onode.manager.getScope("a")
+							rInst := scope.instances[localInst.InstanceID]
+							if !c.c.Check(NewInstanceIDSet(rInst.Dependencies).Equal(NewInstanceIDSet(localInst.Dependencies)), gocheck.DeepEquals, true) {
+								fmt.Printf("%+v\n", NewInstanceIDSet(rInst.Dependencies))
+								fmt.Printf("%+v\n", NewInstanceIDSet(localInst.Dependencies))
+							}
+						}
+						os.Exit(-1)
+					}
+				}
+			}
+
+			// unlock all scopes
+			for _, scope := range scopes {
+				scope.lock.Unlock()
 			}
 		}
+
 		sizes := make([]int, len(c.nodes))
 		allGood := true
 		for i, node := range c.nodes {
@@ -310,6 +444,7 @@ func (c *opsCtrl) reactor() {
 			c.c.Logf("All instructions processed")
 			return
 		}
+		c.completed = len(instructionsSet)
 	}
 }
 
@@ -319,8 +454,12 @@ func newCtrl(r *rand.Rand, nodes []*mockNode, c *gocheck.C) *opsCtrl {
 		c: c,
 		random: r,
 		nodes: nodes,
-		msgChan: make(chan opsMsgSendEvent, 1000),  // should this be buffered?
+		msgChan: make(chan opsMsgSendEvent, 100000),  // should this be buffered?
 		timeoutChan: make(chan opsTimeoutEvent, 1000),  // should this be buffered?
+	}
+
+	for i, node := range nodes {
+		fmt.Println("Node", i, node.id)
 	}
 
 	// patch nodes
@@ -341,12 +480,14 @@ type ConsensusIntegrationTest struct {
 	ctrl *opsCtrl
 	random *rand.Rand
 	oldTimeoutHandler func(d time.Duration) <-chan time.Time
+	seedVal int64
 }
 
 var _ = gocheck.Suite(&ConsensusIntegrationTest{})
 
 func (s *ConsensusIntegrationTest) SetUpSuite(c *gocheck.C) {
 	s.baseReplicaTest.SetUpSuite(c)
+	s.numNodes = *_test_replicas
 	s.oldTimeoutHandler = consensusTimeoutEvent
 	if !*_test_integration {
 		c.Skip("-integration not provided")
@@ -359,44 +500,84 @@ func (s *ConsensusIntegrationTest) TearDownSuite(c *gocheck.C) {
 }
 
 func (s *ConsensusIntegrationTest) SetUpTest(c *gocheck.C) {
-	var seed int64
 	if *_test_seed != 0 {
-		seed = *_test_seed
-		c.Log("Using seed arg: ", seed)
+		s.seedVal = *_test_seed
+		c.Log("Using seed arg: ", s.seedVal)
 	} else {
-		seed = time.Now().Unix()
+		s.seedVal = time.Now().Unix()
 	}
-	s.random = rand.New(rand.NewSource(seed))
+	rand.Seed(s.seedVal)
+	s.random = rand.New(rand.NewSource(s.seedVal))
 
-	c.Log("ConsensusIntegrationTest seeded with: ", seed)
+	c.Log("ConsensusIntegrationTest seeded with: ", s.seedVal)
 
 	s.baseReplicaTest.SetUpTest(c)
 	s.ctrl = newCtrl(s.random, s.nodes, c)
+}
+
+func (s *ConsensusIntegrationTest) runTest(c *gocheck.C) {
+	oldPreacceptTimeout := PREACCEPT_TIMEOUT
+	PREACCEPT_TIMEOUT = PREACCEPT_TIMEOUT / 2
+	oldPreacceptCommitTimeout := PREACCEPT_COMMIT_TIMEOUT
+	PREACCEPT_COMMIT_TIMEOUT = PREACCEPT_COMMIT_TIMEOUT / 2
+	oldAcceptTimeout := ACCEPT_TIMEOUT
+	ACCEPT_TIMEOUT = ACCEPT_TIMEOUT / 2
+	oldAcceptCommitTimeout := ACCEPT_COMMIT_TIMEOUT
+	ACCEPT_COMMIT_TIMEOUT = ACCEPT_COMMIT_TIMEOUT / 2
+	oldPrepareTimeout := PREPARE_TIMEOUT
+	PREPARE_TIMEOUT = PREPARE_TIMEOUT / 2
+	oldPrepareCommitTimeout := PREPARE_COMMIT_TIMEOUT
+	PREPARE_COMMIT_TIMEOUT = PREPARE_COMMIT_TIMEOUT / 2
+	oldBallotFailureWaitTime := BALLOT_FAILURE_WAIT_TIME
+	BALLOT_FAILURE_WAIT_TIME = BALLOT_FAILURE_WAIT_TIME / 2
+	oldExecuteTimeout := EXECUTE_TIMEOUT
+	EXECUTE_TIMEOUT = EXECUTE_TIMEOUT / 2
+	defer func() {
+		PREACCEPT_TIMEOUT = oldPreacceptTimeout
+		PREACCEPT_COMMIT_TIMEOUT = oldPreacceptCommitTimeout
+		ACCEPT_TIMEOUT = oldAcceptTimeout
+		ACCEPT_COMMIT_TIMEOUT = oldAcceptCommitTimeout
+		PREPARE_TIMEOUT = oldPrepareTimeout
+		PREPARE_COMMIT_TIMEOUT = oldPrepareCommitTimeout
+		BALLOT_FAILURE_WAIT_TIME = oldBallotFailureWaitTime
+		EXECUTE_TIMEOUT = oldExecuteTimeout
+	}()
+
+
+	semaphore := make(chan bool, *_test_concurrent_queries)
+	numQueries := *_test_queries
+	wg := sync.WaitGroup{}
+	wg.Add(numQueries)
+	errors := 0
+	for i:=0; i<numQueries; i++ {
+		c.Logf("Iteration %v", i)
+		manager := s.nodes[s.random.Int() % len(s.nodes)].manager
+		instructions := []*store.Instruction{store.NewInstruction("set", "a", []string{fmt.Sprint(i)}, time.Now())}
+		go func() {
+			semaphore <- true
+			_, err := manager.ExecuteQuery(instructions)
+			if err != nil {
+				fmt.Printf("FAILED QUERY: %v", err)
+				errors++
+			}
+			<- semaphore
+			wg.Done()
+		}()
+		runtime.Gosched()
+	}
+	wg.Wait()
+	for len(semaphore) > 0 {
+		time.Sleep(time.Duration(1) * time.Millisecond)
+	}
+	c.Logf("%v queries completed with %v failed client requests\n", numQueries, errors)
+	c.Logf("Test completed with seed: %v\n", s.seedVal)
 }
 
 // tests the operation of an egalitarian paxos cluster
 // without any communication failures between nodes
 func (s *ConsensusIntegrationTest) TestSuccessCase(c *gocheck.C) {
 	c.Log("Testing success case")
-	semaphore := make(chan bool, *_test_concurrent_queries)
-	wg := sync.WaitGroup{}
-	wg.Add(*_test_queries)
-	for i:=0; i<*_test_queries; i++ {
-		c.Logf("Iteration %v", i)
-		manager := s.nodes[s.random.Int() % len(s.nodes)].manager
-		instructions := []*store.Instruction{store.NewInstruction("set", "a", []string{fmt.Sprint(i)}, time.Now())}
-//		manager.ExecuteQuery(instructions)
-		go func() {
-			semaphore <- true
-			manager.ExecuteQuery(instructions)
-			wg.Done()
-			<- semaphore
-		}()
-	}
-	for len(semaphore) > 0 {
-		time.Sleep(time.Duration(1) * time.Millisecond)
-	}
-
+	s.runTest(c)
 }
 
 // tests the operation of an egalitarian paxos cluster
@@ -404,11 +585,5 @@ func (s *ConsensusIntegrationTest) TestSuccessCase(c *gocheck.C) {
 func (s *ConsensusIntegrationTest) TestFailureCase(c *gocheck.C) {
 	c.Log("Testing failure case")
 	s.ctrl.simulateFailures = true
-	for i:=0; i<*_test_queries; i++ {
-		c.Logf("Iteration %v", i)
-		manager := s.nodes[s.random.Int() % len(s.nodes)].manager
-		instructions := []*store.Instruction{store.NewInstruction("set", "a", []string{fmt.Sprint(i)}, time.Now())}
-		manager.ExecuteQuery(instructions)
-		// TODO: don't send instructions to partitioned nodes
-	}
+	s.runTest(c)
 }
