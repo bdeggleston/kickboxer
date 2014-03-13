f7d6f0248376c5f0fc578397c60fc55138afeccc
Blake Eggleston <bdeggleston@gmail.com>
2014-03-12 20:29:31 -0700

    adding tests around instance map

--- src/consensus/instance_test.go
+++ src/consensus/instance_test.go
@@ -47,6 +47,90 @@ func (s *InstanceSetTest) TestSetUnion(c *gocheck.C) {
 	c.Assert(len(set2), gocheck.Equals, 3)
 }
 
+type InstanceMapTest struct { }
+
+var _ = gocheck.Suite(&InstanceMapTest{})
+
+func (s *InstanceMapTest) TestGetAndSet(c *gocheck.C) {
+	imap := NewInstanceMap()
+	instance1 := makeInstance(node.NewNodeId(), []InstanceID{})
+	instance2 := makeInstance(node.NewNodeId(), []InstanceID{})
+	instance3 := makeInstance(node.NewNodeId(), []InstanceID{})
+	instance4 := makeInstance(node.NewNodeId(), []InstanceID{})
+	instance5 := makeInstance(node.NewNodeId(), []InstanceID{})
+
+	c.Assert(imap.Get(instance1.InstanceID), gocheck.IsNil)
+	c.Assert(imap.Get(instance2.InstanceID), gocheck.IsNil)
+	c.Assert(imap.Get(instance3.InstanceID), gocheck.IsNil)
+	c.Assert(imap.Get(instance4.InstanceID), gocheck.IsNil)
+	c.Assert(imap.Get(instance5.InstanceID), gocheck.IsNil)
+
+	imap.Add(instance1)
+	c.Assert(imap.Get(instance1.InstanceID), gocheck.NotNil)
+	c.Assert(imap.Get(instance2.InstanceID), gocheck.IsNil)
+	c.Assert(imap.Get(instance3.InstanceID), gocheck.IsNil)
+	c.Assert(imap.Get(instance4.InstanceID), gocheck.IsNil)
+	c.Assert(imap.Get(instance5.InstanceID), gocheck.IsNil)
+
+	imap.Add(instance2)
+	imap.Add(instance3)
+	imap.Add(instance4)
+	imap.Add(instance5)
+
+	c.Assert(imap.Get(instance1.InstanceID), gocheck.NotNil)
+	c.Assert(imap.Get(instance2.InstanceID), gocheck.NotNil)
+	c.Assert(imap.Get(instance3.InstanceID), gocheck.NotNil)
+	c.Assert(imap.Get(instance4.InstanceID), gocheck.NotNil)
+	c.Assert(imap.Get(instance5.InstanceID), gocheck.NotNil)
+
+	c.Assert(imap.Contains(instance1), gocheck.Equals, true)
+	c.Assert(imap.Contains(instance2), gocheck.Equals, true)
+	c.Assert(imap.Contains(instance3), gocheck.Equals, true)
+	c.Assert(imap.Contains(instance4), gocheck.Equals, true)
+	c.Assert(imap.Contains(instance5), gocheck.Equals, true)
+
+	c.Assert(imap.ContainsID(instance1.InstanceID), gocheck.Equals, true)
+	c.Assert(imap.ContainsID(instance2.InstanceID), gocheck.Equals, true)
+	c.Assert(imap.ContainsID(instance3.InstanceID), gocheck.Equals, true)
+	c.Assert(imap.ContainsID(instance4.InstanceID), gocheck.Equals, true)
+	c.Assert(imap.ContainsID(instance5.InstanceID), gocheck.Equals, true)
+
+	c.Assert(imap.Len(), gocheck.Equals, 5)
+
+	iidSet := NewInstanceIDSet(imap.InstanceIDs())
+	c.Assert(iidSet.Contains(instance1.InstanceID), gocheck.Equals, true)
+	c.Assert(iidSet.Contains(instance2.InstanceID), gocheck.Equals, true)
+	c.Assert(iidSet.Contains(instance3.InstanceID), gocheck.Equals, true)
+	c.Assert(iidSet.Contains(instance4.InstanceID), gocheck.Equals, true)
+	c.Assert(iidSet.Contains(instance5.InstanceID), gocheck.Equals, true)
+
+	instances := imap.Instances()
+	iidSet = NewInstanceIDSet([]InstanceID{})
+	for _, inst := range instances {
+		iidSet.Add(inst.InstanceID)
+	}
+	c.Assert(iidSet.Contains(instance1.InstanceID), gocheck.Equals, true)
+	c.Assert(iidSet.Contains(instance2.InstanceID), gocheck.Equals, true)
+	c.Assert(iidSet.Contains(instance3.InstanceID), gocheck.Equals, true)
+	c.Assert(iidSet.Contains(instance4.InstanceID), gocheck.Equals, true)
+	c.Assert(iidSet.Contains(instance5.InstanceID), gocheck.Equals, true)
+}
+
+func (s *InstanceMapTest) TestGetOrSet(c *gocheck.C) {
+	imap := NewInstanceMap()
+	instance1 := makeInstance(node.NewNodeId(), []InstanceID{})
+	instance2 := makeInstance(node.NewNodeId(), []InstanceID{})
+	instance2.InstanceID = instance1.InstanceID
+
+	instance, existed := imap.GetOrSet(instance1, nil)
+	c.Assert(existed, gocheck.Equals, false)
+	c.Assert(instance, gocheck.Equals, instance1)
+
+	instance, existed = imap.GetOrSet(instance2, nil)
+	c.Assert(existed, gocheck.Equals, true)
+	c.Assert(instance, gocheck.Equals, instance1)
+}
+
 type InstanceSerializationTest struct { }
 
 var _ = gocheck.Suite(&InstanceSerializationTest{})
