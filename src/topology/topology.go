8cca715c4db05810bea8e2bd5eb420048aa33dc0
Blake Eggleston <bdeggleston@gmail.com>
2014-05-17 08:26:59 -0700

    adding topology manager and tests

--- src/topology/topology.go
+++ src/topology/topology.go
@@ -1,4 +1,122 @@
 /**
 Contains interfaces and structs used for managing cluster topology
- */
+*/
 package topology
+
+import (
+	"fmt"
+	"sync"
+)
+
+import (
+	"node"
+	"partitioner"
+)
+
+// contains all of the logic and data
+// for answering questions about the current
+// topology, and routing keys to nodes
+type Topology struct {
+	partitioner       partitioner.Partitioner
+	localNodeID       node.NodeId
+	localDcID         DatacenterID
+	replicationFactor uint
+
+	rings map[DatacenterID]*Ring
+	lock  sync.RWMutex
+}
+
+func NewTopology(
+	localNID node.NodeId,
+	localDCID DatacenterID,
+	prtnr partitioner.Partitioner,
+	replicationFactor uint,
+) *Topology {
+	return &Topology{
+		localNodeID:       localNID,
+		localDcID:         localDCID,
+		partitioner:       prtnr,
+		replicationFactor: replicationFactor,
+		rings: make(map[DatacenterID]*Ring, 1),
+	}
+}
+
+func (t *Topology) AddNode(n Node) error {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	dcId := n.GetDatacenterId()
+	if _, exists := t.rings[dcId]; !exists {
+		t.rings[dcId] = NewRing()
+	}
+	return t.rings[dcId].AddNode(n)
+}
+
+func (t *Topology) Size() int {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+
+	num := 0
+	for _, ring := range t.rings {
+		num += ring.Size()
+	}
+	return num
+}
+
+func (t *Topology) AllNodes() []Node {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+
+	nodes := make([]Node, 0, t.Size())
+	for _, ring := range t.rings {
+		nodes = append(nodes, ring.AllNodes()...)
+	}
+	return nodes
+}
+
+func (t *Topology) GetRing(dcId DatacenterID) (*Ring, error) {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+
+	ring, exists := t.rings[dcId]
+	if !exists {
+		return nil, fmt.Errorf("Unknown datacenter [%v]", dcId)
+	}
+	return ring, nil
+}
+
+func (t *Topology) GetToken(key string) partitioner.Token {
+	return t.partitioner.GetToken(key)
+}
+
+// returns a map of datacenter ids -> replica nodes
+func (t *Topology) GetNodesForToken(tk partitioner.Token) map[DatacenterID][]Node {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+
+	// allocate an additional space for the local node when this is used in queries
+	nodes := make(map[DatacenterID][]Node, len(t.rings)+1)
+	for dcid, ring := range t.rings {
+		nodes[dcid] = ring.GetNodesForToken(tk, uint32(t.replicationFactor))
+	}
+
+	return nodes
+}
+
+// returns local dc replicas for the given token
+func (t *Topology) GetLocalNodesForToken(tk partitioner.Token) []Node {
+	t.lock.RLock()
+	defer t.lock.RUnlock()
+
+	return t.rings[t.localDcID].GetNodesForToken(tk, uint32(t.replicationFactor))
+}
+
+// returns true if the given token is replicated by the local node
+func (t *Topology) TokenLocallyReplicated(tk partitioner.Token) bool {
+	for _, n := range t.GetLocalNodesForToken(tk) {
+		if n.GetId() == t.localNodeID {
+			return true
+		}
+	}
+	return false
+}
